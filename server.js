b4b95c3484a52ce19afbb220109bd61e062770a3
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-06 03:58:09 -0300

    02-Sending a response

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const http = require('http');
+const fs = require('fs')
+
+// let HTML = fs.readFileSync(`${__dirname}/index.html`)
+const names = ["francis","james","Rob"]
+const cars = {
+    name:"Ford",
+    model:"Fiesta"
+}
 
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'})
-    res.end('Hello dude, i am a response')
+    res.writeHead(200,{'Content-Type':'application/json'});
+    const json = JSON.stringify({
+        names,
+        cars
+    })
+    res.end(json)
 })
 
 
