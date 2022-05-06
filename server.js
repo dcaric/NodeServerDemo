05edd212dee82eb1cb246028a9cd9696b442fdf5
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-06 04:51:45 -0300

    05-queryS and params

--- server.js
+++ server.js
@@ -11,12 +11,29 @@ app.get('/',(req,res)=>{
     `)
 });
 
-app.get('/api/user',(req,res)=>{
+app.get('/api/:user/:id',(req,res)=>{
+    let id = req.params.id;
+    let user = req.params.user
+    res.send(`
+        <html>
+            <body>
+                <h1>The user name is ${user} and the id is ${id}</h1>
+            </body>
+        </html>
+    `)
+})
+
+///  hhh.com/car?brand=ford&year=2022
+app.get('/api/car',(req,res)=>{
+    let brand = req.query.brand;
+    let year = req.query.year;
+
     res.send({
-        name:"Francis",
-        lastname:"Jones"
+        brand,
+        year
     })
 })
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
