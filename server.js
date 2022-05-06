f87cd4d41fb254e85b813df9c2d7f86aeba6cdd4
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-06 05:25:02 -0300

    07-Middleware 2

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use('/css',express.static(__dirname+'/public/css'));
+app.use('/',(req,res,next)=>{
+    console.log('someone made a request for:'+ req.url)
+    res.cookie('cookieName','cookieValue')
+    next();
+})
 
 
 app.get('/',(req,res)=>{
